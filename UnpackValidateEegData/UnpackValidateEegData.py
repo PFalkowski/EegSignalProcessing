8724ab1bb868f328e8522eee4610f1f1a4d5ba40
Piotr Falkowski <falcon777@wp.pl>
2019-04-01 00:07:41 +0200

    Add project files.

---
+++ UnpackValidateEegData/UnpackValidateEegData.py
@@ -0,0 +1,57 @@
+import mne
+import matplotlib
+import zipfile
+import os
+import hashlib
+import zipfile
+from os import listdir
+from os.path import isfile, join
+
+inputDir = 'E:\EEG Data'
+outputDir = 'E:\EEG Data'
+zipExtension = '.zip'
+
+def get_files_from_directory(dir, extension):
+    if not os.path.isdir(dir) or not os.path.exists(dir):
+        raise ValueError(f'Directory {dir} does not exist.')
+    files = [join(dir, f) for f in listdir(dir) if f.endswith(extension) and isfile(join(dir, f))]
+    return files
+
+def ComputeFileSha256(fileName):
+    hash = hashlib.sha256()
+    with open(fileName, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+def ValidateFiles(fileNamesList):
+    hashDictionary = {}
+    for fileName in fileNamesList:
+        hashDictionary[fileName] = ComputeFileSha256(fileName)
+    return hashDictionary
+
+def ExtractZipArchive(fileName, directory_to_extract_to): 
+    with zipfile.ZipFile(fileName, 'r') as zipObj:
+        zipObj.extractall(directory_to_extract_to)
+
+def ExtractAllFiles(fileNamesList, directory_to_extract_to):
+    for fileName in fileNamesList:
+        ExtractZipArchive(fileName, directory_to_extract_to)
+
+def GetBasicInfo(filePath):
+    raw_data = mne.io.read_raw_brainvision(filePath, preload=True, stim_channel=False)
+    numpy_array = raw_data._data
+    channel_list = raw_data.ch_names
+    return raw_data.info
+
+    
+
+files = get_files_from_directory(inputDir, zipExtension)
+#filesHashes = ValidateFiles(files)
+#ExtractAllFiles(files, outputDir)
+
+
+path = "E:\EEG Data\Sub01\Session010\Sub01_Session0101_AnestheticInjection.vhdr"
+info = GetBasicInfo(path)
+print(info)
+matplotlib.use('Qt5Agg')
